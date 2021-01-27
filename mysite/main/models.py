f5763f53e2cb5582690408062e0ed6184abb2932
macbookair <m2003@tut.by>
2021-01-27 23:57:37 +0300

    1

--- mysite/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Task(models.Model):
-    title = models.CharField('Название', max_length=60)
-    task = models.TextField('Описание')
-
-    def __str__(self):
-        return self.title
